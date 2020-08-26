8d36ab43b6373019978d380f658a3f729f789618
Deepak <deepak.negi343@gmail.com>
2020-08-26 11:49:25 +0530

    Updates

---
+++ precache-manifest.7f1185afc0049a138c80802e8352cc75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed4026c2f12e0736f8c6be5335eddc9",
+    "url": "/ToolTip_react/index.html"
+  },
+  {
+    "revision": "a2ad4dafe78c878b8eac",
+    "url": "/ToolTip_react/static/css/main.22a3375b.chunk.css"
+  },
+  {
+    "revision": "8b3402b2317bac2f07af",
+    "url": "/ToolTip_react/static/js/2.7ddb2575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToolTip_react/static/js/2.7ddb2575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ad4dafe78c878b8eac",
+    "url": "/ToolTip_react/static/js/main.0f1e6484.chunk.js"
+  },
+  {
+    "revision": "b862d36ebfbeb9d3cce4",
+    "url": "/ToolTip_react/static/js/runtime-main.f41ae7ef.js"
+  }
+]);
